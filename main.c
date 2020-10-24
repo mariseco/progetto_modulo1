b7b699f8d26418ae4c03b79ab059e7d32e59d278
Francescomele <francesco.mele@sns.it>
2020-10-24 17:44:17 +0200

    adding global variables

--- main.c
+++ main.c
@@ -10,26 +10,46 @@
 #include<math.h>
 #include<time.h>
 int field[100][100];
+int iflag;          //partenza caldo(1)/freddo(0)/precedente(altro)
+int measures       //numero di misure
+int i_decorrel     //updating fra una misura e l'altra
+float extfield       //valore del campo magnetico esterno
+float beta           //valore di 1/(kT) = beta
+int L;
+float beta;
 
 
 void my_fscanf(FILE * input, int * x);
-FILE * myfopen(char *name, char *mode);
+FILE * myfopen(char *name);
 
 int main()
 {
-    int L;
-    FILE *input;
+   //leggo file
+    FILE *finput;
+    finput = fopen("input_ising.txt", "r");
+    if (finput==NULL)
+    {
+        perror("Errore in apertura del file");
+        exit(1);
+    }
+   //leggo gli input
+    fscanf(finput, "%d\n", &L);
+    fscanf(finput, "%f\n", &beta);
+    fscanf(finput, "%d\n", &iflag);
+
     
-    input= myfopen("input_ising.txt", "r");
-   
     
-    
-    my_fscanf(input, &L);
-    
-    printf("%d \n",L);
-    fclose(input);
+    fclose(finput);
     return 0;
 }
+
+
+
+
+
+
+
+
 
 
 void my_fscanf(FILE * input, int * x)
@@ -39,7 +59,7 @@ void my_fscanf(FILE * input, int * x)
     
 }
 
-FILE * myfopen(char *name, char *mode)
+FILE * myfopen(char *name)
 {
     FILE * f ;
     f = fopen("input_ising.txt", "r");
